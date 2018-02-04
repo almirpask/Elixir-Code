8193e96556b277646f574c6fd121de26e7faa63a
almirpask <almirpask@hotmail.com>
2018-02-04 21:00:17 -0200

    patter matching example

---
+++ patter_matching.ex
@@ -0,0 +1,23 @@
+{:car, model}  = {:car, "Honda"}
+{_, number} = {"foo", 400}
+{_, _, number2} = {1,2, 88}
+
+
+IO.puts model
+IO.puts number
+IO.puts number2
+
+
+[a, b] =  [1, 2]
+
+IO.puts a + b
+
+[a, b] =  [3, 4]
+
+IO.puts "a = #{a}  b = #{b}"
+
+[^a, b] =  [5, 6]
+
+
+
+
