2e4d51857776e925c261038bfe9d64c7bdf92426
almirpask <almirpask@hotmail.com>
2018-02-08 22:06:51 -0200

    exercice

---
+++ exercice/product.ex
@@ -0,0 +1,11 @@
+defmodule Product do
+    defstruct name: nil, value: 1.99
+end
+
+##On Iex product.ex
+
+# soap = %Product{name: "Dove"}
+# IO.puts soap
+
+# shampoo = %Product{name: "Nivia", value: "5.50"}
+# shampoo
